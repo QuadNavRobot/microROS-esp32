d3a92d2bf62519f946d6e36382aecdd070231b3c
FabioGazzoni <fabio.gazzoni@mi.unc.edu.ar>
2024-04-09 10:30:38 -0300

    Se agrego un modo debug simple que aisle microROS con el resto de las tareas de la placa

--- main/inc/main.h
+++ main/inc/main.h
@@ -23,6 +23,7 @@
 #endif
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+#define DEBUG_MODE 1
 
 // SPI pins
 #define GPIO_MOSI 12  // 38 vision bonnet
